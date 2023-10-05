323bdf116077dc18a2f7652f3e17c0f91a0f7a05
Aidar <ar.ibraev98@gmail.com>
2023-10-05 16:49:40 +0600

    Add additional buttons and events

--- project.js
+++ project.js
@@ -1,6 +1,16 @@
 const container = document.querySelector('.container');
 const resetBtn = document.querySelector('.reset')
-const sizeOfGrid = 4;
+const clearBtn = document.querySelector('.clearIt')
+const blackBtn = document.querySelector('.black')
+const rgbBtn = document.querySelector('.rgb')
+const sizeOfGrid = 8;
+
+function  createRgb () {
+    const r = Math.floor(Math.random() * 256) 
+    const g = Math.floor(Math.random() * 256) 
+    const b = Math.floor(Math.random() * 256) 
+    return [r, g, b]
+}
 
 function createGrid (amtOfGrids) {
     for (let i = 0; i < amtOfGrids; i++) {
@@ -8,11 +18,27 @@ function createGrid (amtOfGrids) {
         rows.classList.add('grid-row')
 
         for (let j = 0; j < amtOfGrids; j++) {
+            const widthAndHeight = 960 / amtOfGrids
             const gridBox = document.createElement('div')
             gridBox.classList.add('grid-box')
+            gridBox.style.width = `${widthAndHeight}px`
+            gridBox.style.height = `${widthAndHeight}px`
             gridBox.addEventListener('mouseover', () => {
-                gridBox.style.backgroundColor = 'black'
+            gridBox.style.backgroundColor = ''
             });
+
+            blackBtn.addEventListener('click', () => {
+            gridBox.addEventListener('mouseenter', () => {
+            gridBox.style.backgroundColor = 'black'
+            })
+            });
+            rgbBtn.addEventListener('click', () => {
+            const [r, g, b] = createRgb()
+            gridBox.addEventListener('mouseenter', () => {
+            const bgColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+            gridBox.style.backgroundColor = bgColor
+            })    
+            })
             rows.appendChild(gridBox)
         }
         container.appendChild(rows)
@@ -30,3 +56,14 @@ resetBtn.addEventListener('click', () => {
     
 
 createGrid(sizeOfGrid)
+
+
+// Function resets grid
+function resetGrid () {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild)
+    }
+    createGrid(sizeOfGrid)
+}
+
+clearBtn.addEventListener('click', resetGrid)
